1ec14c5dd1e6f2ea4a99dc3d9f927a3d1c09b7b6
Adam <adamdhoskinson@gmail.com>
2018-06-25 16:25:16 +0100

    Task-1 Added a Task Class. Also added a Task functions static class and made a menu system.

--- AdamsToDoConsoleApplication/Program.cs
+++ AdamsToDoConsoleApplication/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdamsToDoConsoleApplication
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var taskCollection = new List<Task> { new Task("Example", "This is an example") };
+
+            Console.Write($"Main Menu\n" +
+                $"1. Create\n" +
+                $"2. Edit\n" +
+                $"3. Delete\n");
+
+            var input = Console.ReadLine().ToString();
+
+            Menu.ShowMenu(input);
+
+            if (input == "0")
+            {
+                TaskFunctions.CreateTask(taskCollection);
+            }
+
+            Console.WriteLine("Do you want to exit?  y/n");
+
+            input = Console.ReadLine().ToString();
+
+            if (input == "y")
+            {
+                Environment.Exit(0);
+            }
+
+            input = null;
+
+            Menu.ShowMenu(input);
         }
     }
+
+    public static class Menu
+    {
+        public static void ShowMenu(string input)
+        {
+            while (input != "0" && input != "1" && input != "2")
+            {
+                Console.Write($"Main Menu\n" +
+                $"1. Create\n" +
+                $"2. Edit\n" +
+                $"3. Delete\n");
+
+                input = Console.ReadLine().ToString();
+            }
+        }
 }
+}
+
---
+++ AdamsToDoConsoleApplication/TaskFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamsToDoConsoleApplication
+{
+    public static class TaskFunctions
+    {
+        public static void CreateTask(List<Task> taskCollection)
+        {
+            Console.WriteLine("Type in the Title:\n");
+            var inputTitle = Console.ReadLine();
+
+            Console.WriteLine("Type in the Description:\n");
+            var inputDescription = Console.ReadLine();
+
+            var task = new Task(inputTitle, inputDescription);
+
+            taskCollection.Add(task);
+        }
+    }
+}
